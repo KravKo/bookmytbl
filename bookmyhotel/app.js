44147ffa96e860ee457b5b604d81d54114056e57
KravKo <86314507+KravKo@users.noreply.github.com>
2022-06-18 13:05:22 +0545

    js

---
+++ bookmyhotel/app.js
@@ -0,0 +1,22 @@
+const openmenu = document.getElementById('openmenu');
+const closemenu = document.getElementById('closemenu')
+const upbarr = document.getElementById('upbarr')
+upbarr.style.maxHeight = "0%"
+
+
+function opennn() {
+    console.log("hello world")
+    upbarr.style.maxHeight = "20%"
+    upbarr.style.display = 'block'
+    closemenu.style.display = 'block'
+    openmenu.style.display = 'none'
+
+}
+
+function closeee() {
+    upbarr.style.maxHeight = "0%"
+    upbarr.style.display = 'none'
+    closemenu.style.display = 'none'
+    openmenu.style.display = 'block'
+}
+
